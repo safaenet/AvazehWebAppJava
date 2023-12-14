36c667c2df802046bc4d4c9b881ebe0fadf6a717
safaenet <safa.dh@gmail.com>
2023-12-14 10:47:58 +0330

    first commit

---
+++ src/main/java/com/safadana/AvazehWebApp/AvazehWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.safadana.AvazehWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvazehWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvazehWebAppApplication.class, args);
+		System.out.println("Hi Safa");
+	}
+
+}
---
+++ src/test/java/com/safadana/AvazehWebApp/AvazehWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safadana.AvazehWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvazehWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
