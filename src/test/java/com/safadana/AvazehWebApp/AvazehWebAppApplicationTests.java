5e40648116cb83d2498fa7ebbd4e59b84ae65d47
safaenet <safa.dh@gmail.com>
2024-01-05 00:50:35 +0330

    0

--- src/test/java/com/safadana/AvazehWebApp/AvazehWebAppApplicationTests.java
+++ src/test/java/com/safadana/AvazehWebApp/AvazehWebAppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.safadana.AvazehWebApp;
+// package com.safadana.AvazehWebApp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AvazehWebAppApplicationTests {
+// @SpringBootTest
+// class AvazehWebAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
