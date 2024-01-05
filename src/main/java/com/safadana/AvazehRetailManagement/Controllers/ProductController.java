bf33fc241e9beaef96cef9f649ced5fcd00cd59e
safaenet <safa.dh@gmail.com>
2024-01-05 21:03:48 +0330

    0

--- src/main/java/com/safadana/AvazehRetailManagement/Controllers/ProductController.java
+++ src/main/java/com/safadana/AvazehRetailManagement/Controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.safadana.AvazehRetailManagement.SharedLibrary.DalModels.ItemsForCombo
 import com.safadana.AvazehRetailManagement.SharedLibrary.DalModels.ProductModel;
 import com.safadana.AvazehRetailManagement.SharedLibrary.DalModels.ProductUnitModel;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/Product")
 public class ProductController {
