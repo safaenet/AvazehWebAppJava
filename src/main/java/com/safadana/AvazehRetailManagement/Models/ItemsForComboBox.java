afd66df160342efaeb8e596956979e39ee87bedc
safaenet <safa.dh@gmail.com>
2024-03-10 12:19:18 +0330

    0

--- src/main/java/com/safadana/AvazehRetailManagement/Models/ItemsForComboBox.java
+++ src/main/java/com/safadana/AvazehRetailManagement/Models/ItemsForComboBox.java
@@ -1,9 +1,17 @@
 package com.safadana.AvazehRetailManagement.Models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemsForComboBox {
+    ItemsForComboBox(Long id, String itemName){
+        this.id = id;
+        this.itemName = itemName;
+    }
     private Long id;
     private String itemName;
     private boolean isChecked;
